b5f6f17928ccccd71d9ffcb07a12cafb7f0f524f
YMJK <84662360+fjnuhyk@users.noreply.github.com>
2026-05-18 00:48:11 +0800

    Add files via upload

---
+++ train.py
@@ -0,0 +1,24 @@
+﻿import warnings
+warnings.filterwarnings('ignore')
+from ultralytics import YOLO
+
+
+if __name__ == '__main__':
+    model = YOLO(r'Pepper-Yolo.yaml')
+
+    model.train(data=r'PepperV6.yaml',
+                cache=False,
+                imgsz=640,
+                epochs=300,
+                batch=32,
+                close_mosaic=0,
+                workers=4,
+
+                optimizer='SGD',
+
+
+
+
+                project='runs/train',
+                name='exp',
+                )
